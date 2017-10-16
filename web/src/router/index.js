2854d2c2b496bf66d2748fa2a949a169facf6ac3
Javier Casas <javcasas@gmail.com>
2017-10-15 21:29:20 -0500

    Cleaning up

--- web/src/router/index.js
+++ web/src/router/index.js
@@ -1,11 +1,14 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+
 import Home from '@/components/Home'
 import Catalogo from '@/components/Catalogo'
 import Iluminacion from '@/components/Catalogo/Iluminacion'
 
 import Cart from '@/components/Cart'
 import Checkout from '@/components/Checkout'
+
+import Test from '@/components/Test'
 
 Vue.use(Router)
 
@@ -35,6 +38,11 @@ export default new Router({
       path: '/checkout/',
       name: 'Checkout',
       component: Checkout
+    },
+    {
+      path: '/test/',
+      name: 'Test',
+      component: Test
     }
   ]
 })
